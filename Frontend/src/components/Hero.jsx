6551f55593cc0338b0ba0ad6f30b50c87ca57641
hawi088 <hawi.ugr-4097-16@aau.edu.et>
2026-02-10 20:23:03 +1300

    First Branch

---
+++ Frontend/src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import '../styles/Hero.css'
+function Hero(){
+    return(
+        <section className="hero-section">
+            <div className='left-motto'>Reflect Throughly</div>
+            <div className='right-motto'>Watch Deeply</div>
+        </section>
+    )
+}
+export default Hero
