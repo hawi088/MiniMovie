8b9b3505bd77da0615409078e009f5b448fadfdf
hawi088 <hawi.ugr-4097-16@aau.edu.et>
2026-02-12 22:19:47 +1300

    set up authentication

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import {FavoriteProvider} from '../src/context/FavoritesContext'
-
+import {FavoriteProvider} from '../src/context/FavoritesContext.jsx'
+import {ReviewProvider} from '../src/context/ReviewContext.jsx'
+import { WatchListProvider } from './context/WatchListContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <FavoriteProvider>
+    <ReviewProvider>
+      <WatchListProvider>
     <App />
+    </WatchListProvider>
+    </ReviewProvider>
   </FavoriteProvider>
   </BrowserRouter>
 )
